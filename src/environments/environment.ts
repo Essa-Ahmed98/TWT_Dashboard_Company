5d3bcbc7191a95e122ec0b8e981d76b2d9e32ade
AhMostafaaa <ahmostafaa94@gmail.com>
2026-05-22 23:06:27 +0300

    Switch environment back to dev configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,17 @@
-// export const environment = {
-//   production: false,
-//   apiBase: '/api',
-//   chatApiBase: '/api',
-//   hubUrl: '/api/chat/chatHub',
-//   chatMediaBase: '',
-//   uploadsBase: 'https://API.TWTH.lamarpro.com/',
-// };
-
 export const environment = {
-  production: true,
-  apiBase: 'https://API.TWTH.lamarpro.com/api',
-  chatApiBase: 'https://Chat.TWTH.lamarpro.com/api',
-  hubUrl: 'https://Chat.TWTH.lamarpro.com/api/chat/chatHub',
-  chatMediaBase: 'https://Chat.TWTH.lamarpro.com',
-  uploadsBase: 'https://API.TWTH.lamarpro.com',
+  production: false,
+  apiBase: '/api',
+  chatApiBase: '/api',
+  hubUrl: '/api/chat/chatHub',
+  chatMediaBase: '',
+  uploadsBase: 'https://API.TWTH.lamarpro.com/',
 };
+
+// export const environment = {
+//   production: true,
+//   apiBase: 'https://API.TWTH.lamarpro.com/api',
+//   chatApiBase: 'https://Chat.TWTH.lamarpro.com/api',
+//   hubUrl: 'https://Chat.TWTH.lamarpro.com/api/chat/chatHub',
+//   chatMediaBase: 'https://Chat.TWTH.lamarpro.com',
+//   uploadsBase: 'https://API.TWTH.lamarpro.com',
+// };
